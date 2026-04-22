a6a2bfdce6f705feb2aa3a8ef82e34cd26c8181d
Claryss Mae Pangasian <claryssmaep@gmail.com>
2026-04-22 14:02:38 +0800

    Feature/develop (#36)
    * Auth Logic of sprint 1
    
    * Rights & Auth - Sprint 1
    
    ---------
    
    Co-authored-by: alexzaignacio <alexzagayleadaoignacio@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,30 @@
+<<<<<<< HEAD
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
+import Products from "./pages/Products"; 
+import "./App.css";
+
+function App() {
+  return (
+    <Routes>
+      {/* This sends users to Login by default */}
+      <Route path="/" element={<Navigate to="/login" />} />
+      
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/auth/callback" element={<AuthCallbackPage />} />
+    </Routes>
+  );
+}
+
+export default App;
+
+
+=======
 // src/App.jsx
 import Register from './pages/Register.jsx';
 import './App.css';
@@ -11,3 +38,4 @@ function App() {
 }
 
 export default App;
+>>>>>>> a6691f171f9622c2ccf7159815cd846cbccd17b0
